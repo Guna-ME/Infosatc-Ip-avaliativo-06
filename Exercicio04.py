10cd06c88b3d906ee8a945f49bafa197e39111ee
Guna-ME <Dudamarcos151@hotmail.com>
2020-11-03 22:00:16 -0300

    Feat: adicion exercicio 04

---
+++ Exercicio04.py
@@ -0,0 +1,12 @@
+def parametro():
+    variavel = input("Insira os caracteres {[()]}: ")
+    if "{[()]}" in variavel:
+        print("Parâmetro ok!",True)
+        return True
+    elif "{[()()]}" in variavel:
+        print("Parâmetro ok!",True)
+        return True
+    else:
+        print("Algo deu errado...",False)
+        return False
+parametro()
